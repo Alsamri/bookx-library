d3d6d79b32b2c604eef0a9530c27152d84b6aa2f
Alsamri <azozid@yahoo.com>
2025-04-25 17:26:48 +0100

    added books ui and global types

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,18 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { homedir } from "os";
-
+import BookOverview from "@/components/BookOverview";
+import BookList from "@/components/BookList";
+import { sampleBooks } from "@/constant";
 const Home = () => (
   <>
-    <Button>hey</Button>
+    <BookOverview {...sampleBooks[0]} />
+
+    <BookList
+      title="Latest Books"
+      books={sampleBooks}
+      containerClassName="mt-28"
+    />
   </>
 );
 
